3f50a242ef9573ce8a8146cc7dca9e71ddfcf1fb
SriSomanaath <govindalapudisrinath@gmail.com>
2023-12-14 09:46:45 +0530

    Basic SetUp

---
+++ src/components/index.tsx
@@ -0,0 +1,17 @@
+import About from "./About";
+import Blog from "./Blog";
+import Content from "./Content";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import NavBar from './NavBar';
+import OpenAI from './OpenAI';
+
+export {
+    About,
+    Blog,
+    Content,
+    Footer,
+    Hero,
+    NavBar,
+    OpenAI
+}
