fef7b2e66f945dfd9048ebad849620335217fa4d
SriSomanaath <govindalapudisrinath@gmail.com>
2023-12-14 09:53:30 +0530

    pages.tsx placement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div  className="flex min-h-screen flex-col items-center justify-between p-24">Hero</div>
   )
 }
 
